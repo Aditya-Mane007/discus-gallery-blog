da5268913c0b5804c67b09a5950a906f4de092b1
Aditya <maneaditya006@gmail.com>
2024-11-25 11:46:53 +0530

    Updated Layout.tsx for Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Script from "next/script";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
