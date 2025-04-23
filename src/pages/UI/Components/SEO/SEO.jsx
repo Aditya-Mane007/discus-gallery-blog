502841c49c1b8f5bd6d9d2d8b04559eaca368038
Aditya Ashok Mane <maneaditya006@gmail.com>
2025-04-23 21:24:36 +0530

    Added Test 1 Ads unit

--- src/pages/UI/Components/SEO/SEO.jsx
+++ src/pages/UI/Components/SEO/SEO.jsx
@@ -41,20 +41,6 @@ function SEO({ title, description, imageURL, url }) {
         href="/assets/HeroBackgroundImage.jpg"
         fetchPriority="high"
       />
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3441215479960974"
-        crossorigin="anonymous"
-      ></script>
-      <ins
-        class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-3441215479960974"
-        data-ad-slot="4255819050"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </Head>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,26 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="antialiased">
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3441215479960974"
+          crossorigin="anonymous"
+        ></script>
+        <ins
+          class="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-3441215479960974"
+          data-ad-slot="4255819050"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        `,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
