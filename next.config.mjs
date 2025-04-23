4d5b91cccaccf4e14ee59b419428eae37e26cefe
Aditya Ashok Mane <maneaditya006@gmail.com>
2025-04-23 21:54:16 +0530

    Added Content Security policy in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://www.gstatic.com;
+              img-src 'self' data: https://www.google-analytics.com https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net;
+              connect-src 'self' https://www.google-analytics.com https://pagead2.googlesyndication.com;
+              frame-src https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net;
+            `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
