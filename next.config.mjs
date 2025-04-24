c3e3223866a9b62bcfa6042d4c197cc924ad1e63
Aditya Mane <84222786+Aditya-Mane007@users.noreply.github.com>
2025-04-24 10:24:50 +0530

    Security Headers

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,18 @@ const nextConfig = {
         source: "/(.*)",
         headers: [
           {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Strict-Trasport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
@@ -35,8 +47,16 @@ const nextConfig = {
               .trim(),
           },
           {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
             key: "Permissions-Policy",
-            value: `'self' geolocation`,
+            value: "geolocation=(self)",
           },
         ],
       },
